70507f0d5c96899f9a7b1a6d10ec9c13f545ab66
Pawan Kanodia <p_kanodia32@yahoo.co.in>
2017-12-08 18:04:53 +0530

    Create SelectSql.sql

---
+++ SelectSql.sql
@@ -0,0 +1,2 @@
+SELECT * FROM ip_threshold where duration_type='hourly' and start_date='2017-01-01.15:00:00';
+SELECT * FROM ip_threshold where duration_type='daily' and start_date='2017-01-01.00:00:00';
