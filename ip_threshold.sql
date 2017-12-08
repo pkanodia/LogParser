3ba1e9e9dc45b7825f9a9696866d0da9be443c24
Pawan Kanodia <p_kanodia32@yahoo.co.in>
2017-12-08 17:30:16 +0530

    Create ip_threshold.sql

---
+++ ip_threshold.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS ip_threshold;  
+
+
+CREATE TABLE ip_threshold(           
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,          
+ip_address VARCHAR(100) NOT NULL,   
+request_count INT NOT NULL,   
+comments VARCHAR(500) NOT NULL,   
+duration_type VARCHAR(20) NOT NULL,   
+start_date VARCHAR(20) NOT NULL   )   
